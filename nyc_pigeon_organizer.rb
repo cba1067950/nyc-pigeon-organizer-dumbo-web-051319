323beba8e1508e63446ef2d7864a74409442c02e
Joe Jamsky <cba1067950@yahoo.com>
2019-05-09 22:39:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  nameArray = []
+  sendHash = {}
+  
+  data.each do |trait, dataHash|
+    dataHash.each do |dataKey, dataArray|
+      dataArray.each do |name|
+        sendHash[:pigeon_list] = {name => [(dataKey.to_s)]}   #this overwrites all your previous names
+        binding.pry
+      end
+      binding.pry
+      #nameArray += dataArray
+    end
+    #nameArray = nameArray.uniq
+  end
+  
+  sendHash
 end
