361056dd5f59c00e9eed2c8d1611c0d0f04a7913
Joe Jamsky <cba1067950@yahoo.com>
2019-05-10 00:41:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,19 +7,19 @@ def nyc_pigeon_organizer(data)
   data.each do |trait, dataHash|
     dataHash.each do |dataKey, dataArray|
       dataArray.each do |name|
-        #binding.pry
+
         if sendHash.has_key?(name) == false
           #sendHash = sendHash.merge( {name => { trait => [(dataKey.to_s)] } } )  
           sendHash = sendHash.merge( name => {} )
         end
-        binding.pry
+
         if sendHash["#{name}"].has_key?(trait) == false 
           sendHash = sendHash.merge( {name => { trait => [(dataKey.to_s)] } } )  
         else
           sendHash["#{name}"][trait] << dataKey.to_s  
           #binding.pry
         end
-        #binding.pry
+        binding.pry
       end
       #nameArray += dataArray
     end
