7b3126c97a66b8f5aa93dec064bdf937c4c421d2
Joe Jamsky <cba1067950@yahoo.com>
2019-05-10 00:01:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,18 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  nameArray = []
   sendHash = {}
   
   data.each do |trait, dataHash|
     dataHash.each do |dataKey, dataArray|
       dataArray.each do |name|
+        binding.pry
         if sendHash.has_key?(name) == false
           sendHash = sendHash.merge( {name => { trait => [(dataKey.to_s)] } } )  
           #binding.pry
         else
           sendHash[name][trait] << dataKey.to_s  
-          binding.pry
+          #binding.pry
         end
         #binding.pry
       end
